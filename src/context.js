363ac7641aa535cab683789c50dbfd57081e2358
Cucu Toma Daniel <61708051+DanielTomaCucu@users.noreply.github.com>
2022-08-24 18:23:22 +0300

    Update context.js

--- src/context.js
+++ src/context.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 // make sure to use https
-export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=8455d00e`;
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
 const AppContext = React.createContext();
 console.log(API_ENDPOINT);
 const AppProvider = ({ children }) => {
